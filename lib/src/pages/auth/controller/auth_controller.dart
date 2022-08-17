eb5a4e506a59449d492f3c17dabaecbdbb86a491
Ancora <anderson.rocha@ancora-ti.com.br>
2022-08-17 00:33:18 -0300

    Apresentando toast erro; navegando para tela base.

--- lib/src/pages/auth/controller/auth_controller.dart
+++ lib/src/pages/auth/controller/auth_controller.dart
@@ -1,11 +1,17 @@
+import 'package:ancora_artes/src/models/user_model.dart';
 import 'package:ancora_artes/src/pages/auth/repository/auth_repository.dart';
 import 'package:ancora_artes/src/pages/auth/result/auth_result.dart';
+import 'package:ancora_artes/src/pages_routes/app_pages.dart';
+import 'package:ancora_artes/src/services/utils_services.dart';
 import 'package:get/get.dart';
 
 class AuthController extends GetxController {
   RxBool isLoading = false.obs;
 
   final authRepository = AuthRepository();
+  final utilsServices = UtilsServices();
+
+  UserModel user = UserModel();
 
   Future<void> signIn({
     required String email,
@@ -22,10 +28,16 @@ class AuthController extends GetxController {
 
     result.when(
       success: (user) {
-        print(user);
+        /* this.user refere-se ao objeto da classe; user é do retorno em success */
+        this.user = user;
+        /* navegação */
+        Get.offAllNamed(PagesRoutes.baseRoute);
       },
       error: (message) {
-        print(message);
+        utilsServices.showToast(
+          message: message,
+          isError: true,
+        );
       },
     );
   }
